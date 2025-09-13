7a345b4dd23d1a60b1eb53182b458ce7fddbd23f
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-09-14 00:11:25 +0530

    creating components like hero pricing stats awards

--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        
+        <div className='container'>
+            <div className='row'>
+                <div className='col-4'>
+                    <h1 className='mb-3'>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                     <a className='mx-5' href="" style={{textDecoration:"none"}}>See pricing <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+                <div className='col-2'></div>
+                <div className='col-6'></div>
+            </div>
+        </div>
      );
 }
 
