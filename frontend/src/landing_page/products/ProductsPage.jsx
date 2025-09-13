4f8df6ea5172536b28900e7d035ce5d04d4dd64c
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-09-13 14:44:50 +0530

    creating empty components

---
+++ frontend/src/landing_page/products/ProductsPage.jsx
@@ -0,0 +1,21 @@
+import Footer from "../Footer";
+import Navbar from "../Navbar";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
+
+
+
+function ProductsPage() {
+    return ( <>
+    <Navbar/>
+    <Hero/>
+    <LeftSection/>
+    <RightSection/>
+    <Universe/>
+    <Footer/>
+    </> );
+}
+
+export default ProductsPage;
---
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -0,0 +1,9 @@
+function SupportPage() {
+    return ( 
+        <h1>
+            SupportPage
+        </h1>
+     );
+}
+
+export default SupportPage;
