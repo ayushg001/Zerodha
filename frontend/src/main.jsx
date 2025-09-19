dceee2f803811179e9928e339dba92e82a3af074
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-09-19 23:25:45 +0530

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import HomePage from './landing_page/home/HomePage'
 
 createRoot(document.getElementById('root')).render(
